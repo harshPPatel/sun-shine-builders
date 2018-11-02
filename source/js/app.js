88fe0440b2641afb14cd2a79435878ed49a0b901
harshPPatel <36562266+harshPPatel@users.noreply.github.com>
2018-11-02 13:01:10 -0500

    Commenting in JS is done!

--- source/js/app.js
+++ source/js/app.js
@@ -1,17 +1,29 @@
+//  Variables for JSON Functions for fetching data from the .json file
 var projectsJSONSource = "/assets/js/json/projects.json";
 var upcomingProjectsJSONSource = "/assets/js/json/upcoming-projects.json";
 
 $(document).ready(function() {
 
-  // Function To fill Skill bar using attribute
+  //  Function    : jQuery Function
+  //  Arguments   : index (index for all calsses with name of 'skill-functions')
+  //  Variables   : fillAmount   => fetch the fill up value from the attribut in the HTML document.
+  //  Description : fetch the fill up amaount, apply css rule to the skill-fill class
+
   $('.skill-fill').each(function(index) {
     var fillAmount = $(this).attr("data-fill");
     $(this).css("width", fillAmount + "%");
-    $(this).attr("title", fillAmount);
   });
 
-
 });
+
+
+//  Function    : printProjectObject
+//  Arguments   : projectObject (an object from JSON file)
+//                classname (to catch working and upcoming projects form HTML)
+//  Variables   : divs        => get class for working or upcoming task
+//                printLayout =>  an template to print data
+//  Description : fetch data from JSON functions and print it in the formate of the provided
+//                template.
 
 function printProjectObject(projectObject, classname) {
   var divs = document.getElementsByClassName(classname);
@@ -26,6 +38,13 @@ function printProjectObject(projectObject, classname) {
   }
 }
 
+//  Function    : getProjectData
+//  Arguments   : none
+//  Variables  :  xhttp        => creates new XMLHttpRequest in the function
+//                projectData  => save data in variable from the JSON file
+//  Description : fetch data from projects.json file in the json folder, make and http request,
+//                save it in variable and print the data in provided layout.
+
 function getProjectData() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -37,6 +56,13 @@ function getProjectData() {
   xhttp.open("GET", projectsJSONSource, true);
   xhttp.send();
 }
+
+//  Function    : getUpcomingProjectData
+//  Arguments   : none
+//  Variables  :  xhttp                 => creates new XMLHttpRequest in the function
+//                upcomingProjectData   => save data in variable from the JSON file
+//  Description : fetch data from upcoming-projects.json file in the json folder, make and http request,
+//                save it in variable and print the data in provided layout.
 
 function getUpcomingProjectData() {
 
@@ -51,13 +77,18 @@ function getUpcomingProjectData() {
   xhttp.send();
 }
 
+//  Function    : onLoadFunctions
+//  Arguments   : none
+//  Variables  :  none
+//  Description : List of function to be load when page is loaded
+
 function onLoadFunctions () {
   //JSON Functions
   getProjectData();
   getUpcomingProjectData();
 }
 
+
+// Running onLoadFunctions while page loads
+
 window.onload = onLoadFunctions;
-
-
-// TODO: Do Commenting for whole JS
